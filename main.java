d5a7cc9b5ab9f4585e475beac172c788d472e467
63212474 <632124794@qq.com>
2018-10-04 10:42:26 +0800

    this is a main

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main 
+{public static void main(String[] args) 
+	{String str = "this is Java";
+		System.out.println(removeCharAt(str,3));}
+		public static String removeCharAt(String s , int pos){
+return s.substring(0,pos) +s.substring(pos + 1);
+	}
+}
